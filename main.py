403cd8119a970b192b34b3387d91628d5683a751
kevmacal <kevmacal@espol.edu.ec>
2016-01-09 20:09:38 -0500

    Conversor Tile-Node

--- main.py
+++ main.py
@@ -2,7 +2,34 @@ import pygame
 import sys
 import Tile
 import Board
+import PathFinding
 from pygame.locals import *
+
+def convertLTileToLNode(board):
+    lista = []
+    for i in range(10):
+        a = [0] * 20
+        lista.append(a)
+
+    for i in range(10):
+        for j in range(20):
+            lista[i][j]=convertTiletoNode(board.getBoard()[i][j])
+    return lista
+
+def convertTiletoNode(tile):
+    return PathFinding.Node(pos=[tile.x, tile.y], element=tile)
+
+def getNeighborsListNode(board,nodo):
+    lNeig = []
+    x = nodo.pos[0]
+    y = nodo.pos[1]
+    for i in range(3):
+        for j in range(3):
+            if x - 1 + i >= 0 and x - 1 + i < 10 and y - 1 + j >= 0 and y - 1 + j < 20:
+                if board[x - 1 + i][y - 1 + j].element.getType() != 0:
+                    if x - 1 + i != x or y - 1 + j != y:
+                        lNeig.append(board[x - 1 + i][y - 1 + j])
+    return lNeig
 
 def main():
     pygame.init()
@@ -30,6 +57,10 @@ def main():
     xFug = 3
     yFug = 3
       # cuadro = (xIni + xFug, yIni + yFug, 40, 15)  # iniX,iniY,distX,distY
+    lista = convertLTileToLNode(board)
+    lnei = getNeighborsListNode(lista, PathFinding.Node(pos=[1, 3]))
+    for i in range(len(lnei)):
+        print ((lnei[i].pos))
 
     ventanaP.blit(tablero, (0, 0))
 
